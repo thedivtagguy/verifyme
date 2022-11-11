dfc3c2d505b77e68e28098ceca7a49352029f598
thedivtagguy <amanbhargava2001@gmail.com>
2022-11-11 23:42:02 +0530

    Initial Commit

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,10 +1,9 @@
 <script>
-  export let title = "Aman Bhargava";
+  export let title = "Verify Me";
   export let description =
-    "Aman's platform for a regular mishmash of design and code gimmickry";
-  export let url = "https://thedivtagguy.com";
-  // export let slug = 
-  //   "common/assets/thumbnails/960/social.jpg"
+    "Verification without a disposable income.";
+  export let url = "https://verifyme.fun";
+  export let slug =  "/assets/sharecard.jpg"
 </script>
 
 <svelte:head>
@@ -13,27 +12,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="description" content={description} />
-  <meta name="author" content="Aman Bhargava" />
+  <meta name="author" content="Verify Me" />
   <meta name="news_keywords" content="" />
 
   <meta property="og:title" content={title} />
-  <meta property="og:site_name" content="thedivtagguy" />
+  <meta property="og:site_name" content="verifyme" />
   <meta property="og:url" content={url} />
   <meta property="og:description" content={description} />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <!-- <meta property="og:image" content="https://thedivtagguy.com/{slug}" /> -->
+  <meta property="og:image" content="https://verifyme.fun/{slug}" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="https://thedivtagguy.com" />
-  <meta name="twitter:creator" content="@thedivtagguy" />
+  <meta name="twitter:site" content="https://verifyme.fun" />
+  <meta name="twitter:creator" content="@verifyme" />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <!-- <meta name="twitter:image:src" content="https://thedivtagguy.com/{slug}" /> -->
+  <meta name="twitter:image:src" content="https://verifyme.fun/{slug}" />
 
   <meta name="robots" content="max-image-preview:large" />
 
